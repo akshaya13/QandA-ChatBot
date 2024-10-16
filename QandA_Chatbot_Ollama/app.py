09f13d7c2e0ccffba8a3184412e1f13459518833
Akshaya Barat Bushan <akshayabarat@gmail.com>
2024-10-15 23:51:54 -0400

    Update app.py

--- QandA_Chatbot_Ollama/app.py
+++ QandA_Chatbot_Ollama/app.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 ## Langsmith tracking
 
-os.environ['LANGCHAIN_API_KEY'] = os.getenv("LANGCHAIN_API_KEY")
+langchain_api_key = os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_PROJECT"] = "Q&A Chatbot with Ollama"
 
