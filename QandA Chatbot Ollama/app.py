645bbdbb78f5d00c9a69860ff124c6ef01bcf7e8
Akshaya <akshayabarat@gmail.com>
2024-10-15 23:07:58 -0400

    Ollama and OpenAI ChatBot

---
+++ QandA Chatbot Ollama/app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+from langchain_community.llms import Ollama
+
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+## Langsmith tracking
+
+os.environ['LANGCHAIN_API_KEY'] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "Q&A Chatbot with Ollama"
+
+## Prompt Template
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please respond to the user queries"),
+        ("user", "Question: {question}")
+    ]
+)
+
+def generate_response(question, llm, temperature, max_tokens):    
+    llm = Ollama(model = llm)
+    output_parser = StrOutputParser()
+    chain = prompt | llm | output_parser
+    answer = chain.invoke({'question': question})
+    return answer
+
+## Title
+st.title("Enhanced Q&A Chatbot with Open AI")
+
+## Sidebar for API-KEY
+st.sidebar.title("Settings")
+
+## Dropdown to select the preferred model
+llm = st.sidebar.selectbox("Select an Open AI Model", 
+                           ("llama3", "llama2", "gemma:2b")
+                           )
+
+model_info = {
+    "llama3": "Llama3: Suitable for general queries and conversations.",
+    "llama2": "Llama2: Good balance between performance and accuracy.",
+    "gemma:2b": "Gemma:2b: Optimized for technical and complex questions."
+}
+st.sidebar.write(model_info[llm])
+
+temperature = st.sidebar.slider("Temperature", min_value=0.0, max_value=1.0, value = 0.7)
+
+max_tokens = st.sidebar.slider("Max Tokens", min_value=50, max_value=300, value =150)
+
+
+## Main Interface for User input
+
+st.write("Ask me Anything!")
+user_input = st.text_input("You: ")
+submit_button = st.button("Submit")
+
+
+if submit_button:
+    if user_input:
+        with st.spinner('Generating response...'):
+            response = generate_response(user_input, llm, temperature, max_tokens)
+        st.write(response)
+    else:
+        st.warning("Please provide the query !")
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# ChatBot
-Conversational ChatBot with Message History
+# Q&A ChatBot with Ollama
+A Q&A Chatbot built using Streamlit that allows users to interact with various AI models like Llama3, Llama2, and Gemma:2b for generating responses to user queries. The project integrates LangChain for prompt management and Ollama for accessing different models, providing a flexible and customizable chatbot experience.
+
+## Tech Stack:
+
+Python, Streamlit, LangChain, Ollama (llama3, llama2, gemma:2b), Langsmith
+
+## Features:
+
+* Multiple Model Support: Users can choose between Llama3, Llama2, and Gemma:2b based on their needs.
+* Customizable Responses: Adjustable settings for temperature and max tokens to control response style and length.
+* User-Friendly Interface: Simple input field and submission button for asking queries, with real-time response generation.
+* Environment Configuration: Uses .env files for secure API key management and tracking settings with Langsmith.
+
+# Q&A ChatBot with OpenAI
+
+A Q&A Chatbot application using Streamlit that enables users to interact with OpenAI models like GPT-4. It leverages LangChain's prompt management to streamline query processing and OpenAI's API for generating responses. The app offers customization options for the model's behavior through adjustable parameters.
+
+## Tech Stack:
+Python, Streamlit, OpenAI API, LangChain, dotenv
+
+## Features:
+* Multiple OpenAI Model Selection: Users can select models like GPT-4, GPT-4-Turbo, and GPT-4o based on their preferences.
+* Customizable Output: Adjust temperature and max tokens through the interface to control response creativity and length.
+* Secure API Key Input: Users can securely input their OpenAI API key through the sidebar for accessing the models.
+* User-Friendly Query Input: Provides a simple input field for users to ask questions and receive real-time responses.
